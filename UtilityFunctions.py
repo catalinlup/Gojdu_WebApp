a268248b0bfefe5dae2a821239681311b5e99fed
Catalin Lupau <catalinlup>
2019-04-05 19:52:24 +0300

    Second Commmit

--- UtilityFunctions.py
+++ UtilityFunctions.py
@@ -12,11 +12,11 @@ def LoginDataValid(username,password):
 
 def RegistrationDataValid(username,password,sir_name,given_name,profile,email):
     if manager.usernameExists(username):
-        return "Numele de utilizator deja exista!"
+        return "Numele de utilizator deja există!"
     if len(password) < 8:
-        return "Parola trebuie sa aiba cel putin 8 caractere!"
+        return "Parola trebuie sa aibă cel puțin 8 caractere!"
     if len(sir_name) == 0 or len(given_name) == 0 or len(profile) == 0 or len(email) == 0:
-        return "Completati toate datele!"
+        return "Completați toate datele!"
     return "OK"
 
 def RegisterData(username,password,sir_name,given_name,profile,email,teacher):
@@ -30,27 +30,34 @@ def UpdateSession(session):
 
 def RegisterGroupValid(username,name):
     if len(name) == 0:
-        return "Trebuie sa dati un nume grupului!"
+        return "Trebuie să dați un nume grupului!"
     if manager.groupExists(name,username):
-        print("DA")
-        return "Grupul deja exista!"
+        return "Grupul deja există!"
     return "OK"
 
 def RegisterGroup(username,name):
     manager.addGroup(name,username)
 
+def DeleteGroup(name,owner):
+    manager.deleteGroup(name,owner)
+
 def addMemberToGroup(name,owner,members):
     if manager.usernameExists(members):
         manager.addMembersToGroup(name,owner,members)
         return "OK"
-    return "Nu exista utilizatorul"
+    return "Nu există utilizatorul"
+
+def removeMemberFromGroup(name,owner,member):
+    if manager.usernameExists(member):
+        manager.removeMemberFromGroup(name,owner,member)
+        return "OK"
+    return "Nu există utilizatorul"
 
 def addHomeworksToGroup(name,owner,homeworkname,filename):
     manager.addHomeworksToGroup(name,owner,homeworkname,filename)
 
-def deleteHomeworksFromGroup(name,owner,homeworkfilename):
-    print("DA")
-    manager.deleteHomeworksFromGroup(name,owner,homeworkfilename)
+def deleteHomeworksFromGroup(homeworkfilename):
+    manager.deleteHomeworksFromGroup(homeworkfilename)
 
 def SubmitHomework(username,filename,homeworkfilename):
     manager.SubmitHomework(username,filename,homeworkfilename)
@@ -59,13 +66,10 @@ def GetSubmittedHomeworks(homeworkfilename):
     return manager.GetSubmittedHomeworks(homeworkfilename)
 
 def getGroupMembers(name,owner):
-    return manager.getGroupMembers(name,owner)
+    return sorted(manager.getGroupMembers(name,owner))
 
 def getGroupsTheUserIsIn(username):
-    return manager.getGroupsTheUserIsIn(username)
+    return sorted(manager.getGroupsTheUserIsIn(username))
 
 def getGivenHomeworks(name,owner):
     return manager.getGivenHomeworks(name,owner)
-
-
-
